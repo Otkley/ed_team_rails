83dcaef2af8d82bb34ee8489b1770529a97a0792
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-25 16:00:18 -0600

    Modificamos las rutas por defecto de devise, agregamos un atributo a formulario y current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # Aqui modificamos el path de las rutas por defecto de devise
+  # rails routes | grep user
+  # devise_for :users
+  # /users/sign_in to /login
+  # /users/sign_out to /logout
+  # /users/sign_up to /register
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
+  
   resources :rooms
   root to: 'pages#home'
 
