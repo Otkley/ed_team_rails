2b6f8a6c47bb117d32125b61b5cc73aabb14f525
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-25 18:55:28 -0600

    add description to rooms and bundle i pend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_25_221726) do
+ActiveRecord::Schema.define(version: 2023_11_26_005329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_11_25_221726) do
     t.bigint "hotel_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
   end
 
